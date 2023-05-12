ae14b70cc60d2b6720789c66bb125706763223db
David B <david.banol.3889@eam.edu.co>
2023-05-12 14:03:37 -0500

    Test commit

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -7,6 +7,7 @@ import {Location} from '@angular/common'
 import { Cast} from 'src/app/interfaces/credits.interface';
 import { combineLatest } from 'rxjs';
 
+
 @Component({
   selector: 'app-pelicula',
   templateUrl: './pelicula.component.html',
@@ -17,7 +18,7 @@ export class PeliculaComponent implements OnInit {
   pelicula?:MovieDetails;
   cast:Cast[]=[];
 
-  constructor(config: NgbRatingConfig, private activatedRoute: ActivatedRoute, private peliculasSvc:PeliculasService, private location:Location, private router: Router) { 
+  constructor(config: NgbRatingConfig, private activatedRoute: ActivatedRoute, private peliculasSvc:PeliculasService, private location:Location, private router: Router) {
      // customize default values of ratings used by this component tree
      config.max = 10;
      config.readonly = true;
@@ -25,10 +26,10 @@ export class PeliculaComponent implements OnInit {
 
   ngOnInit() {
     const {id} = this.activatedRoute.snapshot.params;
-    
+
     combineLatest([
       this.peliculasSvc.getPeliculaDetalle(id),
-      this.peliculasSvc.getCast(id)  
+      this.peliculasSvc.getCast(id)
     ]).subscribe(([movie, cast])=>{
 
       if (!movie) {
@@ -46,9 +47,9 @@ export class PeliculaComponent implements OnInit {
       this.router.navigateByUrl('/');
         return;
       }
-      this.pelicula=movie;        
+      this.pelicula=movie;
     });
-    
+
     this.peliculasSvc.getCast(id).subscribe(cast=>{
       this.cast=cast
       console.log(cast)
@@ -59,6 +60,6 @@ export class PeliculaComponent implements OnInit {
 
 this.location.back();
 
-  }  
+  }
 
 }
